f9ae9495f66bc37b5b025dbd067930daee801388
Gaurish Naik <nerdgaurish@gmail.com>
2020-03-27 23:03:23 +0530

    the 1st prototype

---
+++ js/script.js
@@ -0,0 +1,18 @@
+  var typed = new Typed("#typed", {
+    stringsElement: '#typed-strings',
+    typeSpeed: 20,
+    backSpeed: 20,
+    backDelay: 1900,
+    startDelay: 1000,
+    loop: true,
+    
+  });
+  var typed = new Typed("#typedd", {
+    stringsElement: '#typed-string',
+    typeSpeed: 20,
+    backSpeed: 20,
+    backDelay: 2000,
+    startDelay: 1000,
+    loop: true,
+    
+  });
